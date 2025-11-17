7a61efdab3bcc8a3031feb2d5ff31d9b89520486
Muhammad Akbar Kurniawan <ledeaamamiya1408@Gmail.com>
2025-11-17 18:05:22 +0700

    Add dummy skills data to skills.js

---
+++ data/skills.js
@@ -0,0 +1,8 @@
+// Dummy skills data
+export const skills = [
+  { name: "JavaScript", level: 90 },
+  { name: "React", level: 85 },
+  { name: "Tailwind CSS", level: 80 },
+  { name: "Figma", level: 75 },
+  { name: "Node.js", level: 70 },
+];
